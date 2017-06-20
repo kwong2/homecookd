054aa75c30616e375d28ae4d5b2b5d7d0c0d436e
Kevin Wong <k.wong4@yahoo.com>
2017-06-19 22:11:17 -0400

    added registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationsController < ApplicationController
 	protected
 	def update_resource(resource, params)
-		resource.update_without_password(params)[:phone_number,:description]
+		resource.update_without_password(params)
 	end
 end
