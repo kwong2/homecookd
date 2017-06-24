d3237060859c91d472fbbd1555e634057b810cd4
Alfonso Estremadoyro <alfonsoestre24@gmail.com>
2017-06-24 18:16:39 -0400

    "Modified rooms page and reformatted titles."

--- app/models/room.rb
+++ app/models/room.rb
@@ -7,7 +7,6 @@ class Room < ApplicationRecord
   validates :room_type, presence: true
   validates :accommodate, presence: true
   validates :bed_room, presence: true
-  validates :bath_room, presence: true
   validates :listing_name, presence: true, length: {maximum: 50}
   validates :summary, presence: true, length: {maximum: 500}
   validates :address, presence: true
